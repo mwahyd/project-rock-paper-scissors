769962a96b06e89aa5b8d1b8e3855b52c224eb20
mwahyd <maishan.wahyd6@gmail.com>
2022-10-13 12:28:05 +0200

    enable game logic to determine outcome

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function get_computer_choice(choice_list) {
 function get_player_choice(choice_list) {
   let user_response = prompt("rock, paper, scissors? ").trim().toLowerCase();
   if (is_valid_choice(user_response, choice_list)) {
-    console.log(`Player choice ${user_response} is valid`);
+    return user_response;
   } else {
     console.log(`Player choice "${user_response}" invalid`);
   }
@@ -30,4 +30,39 @@ function is_valid_choice(user_response, choice_list) {
   }
 }
 
-get_player_choice(choices);
+function determine_outcome(player_choice, comp_choice) {
+  // TIE outcome
+  if (player_choice == "rock" && comp_choice == "rock") {
+    console.log("It's a TIE! Rock vs Rock");
+  } else if (player_choice == "paper" && comp_choice == "paper") {
+    console.log("It's a TIE! Paper vs Paper");
+  } else if (player_choice == "scissors" && comp_choice == "scissors") {
+    console.log("It's a TIE! Scissors vs Scissors");
+  }
+
+  // Win outcome
+  else if (player_choice == "rock" && comp_choice == "scissors") {
+    console.log("You Win! Rock beats Scissors");
+  } else if (player_choice == "paper" && comp_choice == "rock") {
+    console.log("You Win! Paper beats Rock");
+  } else if (player_choice == "scissors" && comp_choice == "paper") {
+    console.log("You Win! Scissors beats Paper");
+  }
+
+  // Loss outcome
+  else if (player_choice == "scissors" && comp_choice == "rock") {
+    console.log("You Lose! Rock beats Scissors");
+  } else if (player_choice == "rock" && comp_choice == "paper") {
+    console.log("You Lose! Paper beats Rock");
+  } else if (player_choice == "paper" && comp_choice == "scissors") {
+    console.log("You Lose! Scissors beats Paper");
+  }
+}
+
+function play_game(choice_list) {
+  let player1 = get_player_choice(choice_list);
+  let computer = get_computer_choice(choice_list);
+  determine_outcome(player1, computer);
+}
+
+play_game(choices);
